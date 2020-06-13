5c7cd9983ceeadf7d13fac0d6fd8fc6502ace46d
Devashish Nayak <devashishnayakrps@gmail.com>
2020-06-13 23:40:56 +0530

    build

---
+++ precache-manifest.8a1a0815ee8c796246e194d9c8b272bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5ccf61e99f19667d047",
+    "url": "/static/css/main.c6f920b8.chunk.css"
+  },
+  {
+    "revision": "d5ccf61e99f19667d047",
+    "url": "/static/js/main.d5ccf61e.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab0a9c67d48502f13963c6e16ce48db3",
+    "url": "/index.html"
+  }
+];
